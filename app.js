3d367c279596275b0d582e1b30d15e5933abbfde
Roshan Acharya <56434316+coderosh@users.noreply.github.com>
2020-10-01 02:52:51 +0000

    chat style changed

--- app.js
+++ app.js
@@ -1,97 +1,93 @@
-const express = require("express")
-const path = require("path")
-const http = require("http")
-const session = require("express-session")
-const bcrypt = require("bcrypt")
-const socketIo = require("socket.io")
-const flash = require("connect-flash")
-const getMessage = require("./utils/messages")
-const { addUser, removeUser, getUser, getUsersInRoom } = require("./utils/users")
-const { Socket } = require("net")
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const getMessage = require("./utils/messages");
+const {
+  addUser,
+  removeUser,
+  getUser,
+  getUsersInRoom,
+} = require("./utils/users");
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
 
+app.use(express.static(__dirname + "/public"));
 
-
-const app = express()
-const server = http.createServer(app)
-const io = socketIo(server)
-
-
-
-app.use(express.static(__dirname + "/public"))
-
-app.set("view engine", 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
-
-
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-    res.render("index")
-})
+  res.render("index");
+});
 
 app.get("/chat", (req, res) => {
-    res.render("chat")
-})
+  res.render("chat");
+});
 
 io.on("connection", (socket) => {
-    console.log("User connected")
+  console.log("User connected");
 
-    socket.on('join', ({ username, room }, callback) => {
-        const { error, user } = addUser({ id: socket.id, username, room })
-        if (error) {
-            return callback(error)
-        }
-        console.log(socket.connected)
-        socket.join(user.room)
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room });
+    if (error) {
+      return callback(error);
+    }
+    console.log(socket.connected);
+    socket.join(user.room);
 
-        socket.emit('message', getMessage('Admin', "Welcome"))
+    socket.emit("message", getMessage("Admin", "Welcome"));
 
-        socket.broadcast.to(user.room).emit('message', getMessage('Admin', `${user.username} has joined the room`))
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        getMessage("Admin", `${user.username} has joined the room`)
+      );
 
-        io.to(user.room).emit('roomData', {
-            room: user.room,
-            users: getUsersInRoom(user.room)
-        })
-        callback()
-    })
-    //receive message
-    socket.on('send-message', (message, callback) => {
-        const user = getUser(socket.id)
-        io.to(user.room).emit('message', getMessage(user.username, message))
-        callback('Delivered')
-    })
+    io.to(user.room).emit("roomData", {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+    callback();
+  });
+  //receive message
+  socket.on("send-message", (message, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit("message", getMessage(user.username, message));
+    callback("Delivered");
+  });
 
+  //listen on typing
+  // socket.on('typing', data => {
+  //     socket.broadcast.emit('typing', data)
+  // })
 
-    //listen on typing
-    // socket.on('typing', data => {
-    //     socket.broadcast.emit('typing', data)
-    // })
+  //on disconnect
+  socket.on("disconnect", () => {
+    const user = removeUser(socket.id);
+    console.log(user);
+    if (user) {
+      socket
+        .to(user.room)
+        .emit(
+          "notification",
+          getMessage("Admin", `${user.username} left the room!`)
+        );
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+      io.to(user.room).emit(
+        "message",
+        getMessage("Admin", `${user.username} has left!`)
+      );
+    }
+  });
+});
 
-    //on disconnect
-    socket.on("disconnect", () => {
-        const user = removeUser(socket.id)
-        console.log(user)
-        if (user) {
-            socket.to(user.room).emit('notification', getMessage('Admin', `${user.username} left the room!`))
-            io.to(user.room).emit('roomData', {
-                room: user.room,
-                users: getUsersInRoom(user.room)
-            })
-            io.to(user.room).emit('message', getMessage('Admin', `${user.username} has left!`))
-        }
-    })
-})
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log("Server started")
-})
+  console.log("Server started");
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,96 +1,93 @@
-
-const socket = io()
+const socket = io();
 
 //elements
 const $messageForm = document.querySelector("#message-form");
-const $messageFormInput = document.querySelector("#message-form input")
-const $messageFormButton = document.querySelector("#message-form button")
-const $messages = document.querySelector("#messages")
-const $room = document.querySelector(".roomname")
-const $users = document.querySelector(".users")
-$typing = document.querySelector("#feedback")
-
-
-
-
-
-
+const $messageFormInput = document.querySelector("#message-form input");
+const $messageFormButton = document.querySelector("#message-form button");
+const $messages = document.querySelector("#messages");
+const $room = document.querySelector(".roomname");
+const $users = document.querySelector(".users");
+$typing = document.querySelector("#feedback");
 
 //options
 
-const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+const { username, room } = Qs.parse(location.search, {
+  ignoreQueryPrefix: true,
+});
 
 const autoScroll = () => {
-    const $newMessage = $messages.lastElementChild
-    const newMessageStyles = getComputedStyle($newMessage)
-    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+  const $newMessage = $messages.lastElementChild;
+  const newMessageStyles = getComputedStyle($newMessage);
+  const newMessageMargin = parseInt(newMessageStyles.marginBottom);
 
-    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+  const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
 
-    //visible height
-    const visibleHeight = $messages.offsetHeight
+  //visible height
+  const visibleHeight = $messages.offsetHeight;
 
-    //height of container
-    const containerHeight = $messages.scrollHeight
+  //height of container
+  const containerHeight = $messages.scrollHeight;
 
-    //  far  i scrolled
-    const scrolloffSet = $messages.scrollTop + visibleHeight
+  //  far  i scrolled
+  const scrolloffSet = $messages.scrollTop + visibleHeight;
 
-    if (containerHeight - newMessageHeight <= scrolloffSet) {
-        $messages.scrollTop = $messages.scrollHeight
-    }
-
-
-}
+  if (containerHeight - newMessageHeight <= scrolloffSet) {
+    $messages.scrollTop = $messages.scrollHeight;
+  }
+};
 
 //when message are sent
 socket.on("message", (message) => {
-    console.log(message)
-    const html = {
-        username: message.username,
-        message: message.text,
-        createdAt: moment(message.createdAt).format('LT')
-    }
-    const markup = `
-    <div class="chat">
-    <p class="username" style="margin-bottom: 3px; text-transform: capitalize; font-size: 13px; font-weight: bold">${html.username} - <span>${html.createdAt}</span></p>
-    <p id="message" class=${html.username === username ? 'message' : 'newuser'} style="margin-bottom: 0px">${html.message}</p>
+  console.log(message);
+  const html = {
+    username: message.username,
+    message: message.text,
+    createdAt: moment(message.createdAt).format("LT"),
+  };
+  const markup = `
+    <div style="${
+      html.username === username
+        ? "align-self: flex-end;align-items: flex-end"
+        : "align-self: flex-start;align-items: flex-start"
+    }" class="chat">
+    <p class="username" style="margin-bottom: 3px; text-transform: capitalize; font-size: 13px; font-weight: bold">${
+      html.username
+    } - <span>${html.createdAt}</span></p>
+    <p id="message" class=${
+      html.username === username ? "message" : "newuser"
+    } style="margin-bottom: 0px">${html.message}</p>
     </div>
-    `
-    $messages.insertAdjacentHTML('beforeend', markup)
+    `;
+  $messages.insertAdjacentHTML("beforeend", markup);
 
-    autoScroll()
-
-})
+  autoScroll();
+});
 
 //for showing online users
-socket.on('roomData', ({ room, users }) => {
-
-    $room.innerHTML = ` <h3>${room}</h3>`
-    let html = ''
-    users.forEach(user => {
-        html += `
+socket.on("roomData", ({ room, users }) => {
+  $room.innerHTML = ` <h3>${room}</h3>`;
+  let html = "";
+  users.forEach((user) => {
+    html += `
                 <li class="user" style="list-style:none; color: green">${user.username} - online</li>
-        `
-    })
-    $users.innerHTML = html
+        `;
+  });
+  $users.innerHTML = html;
+});
+$messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-})
-$messageForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-
-    $messageFormButton.setAttribute('disabled', 'disabled')
-    const message = $messageFormInput.value
-    socket.emit('send-message', message, (err) => {
-
-        $messageFormButton.removeAttribute('disabled')
-        $messageFormInput.value = ''
-        $messageFormInput.focus()
-        if (err) {
-            console.log(err)
-        }
-    })
-})
+  $messageFormButton.setAttribute("disabled", "disabled");
+  const message = $messageFormInput.value;
+  socket.emit("send-message", message, (err) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    if (err) {
+      console.log(err);
+    }
+  });
+});
 
 // //Emit typing
 // $messageFormInput.addEventListener("keypress", e => {
@@ -109,20 +106,20 @@ $messageForm.addEventListener('submit', (e) => {
 
 // });
 
-socket.emit('join', { username, room }, (error) => {
-    if (error) {
-        alert(error)
-    }
-})
+socket.emit("join", { username, room }, (error) => {
+  if (error) {
+    alert(error);
+  }
+});
 
-socket.on('notification', (message) => {
-    Push.create('Chat Room', {
-        body: message.text,
-        // icon: '/icon.png',
-        timeout: 4000,
-        onClick: function () {
-            window.focus()
-            this.close()
-        },
-    })
-})
+socket.on("notification", (message) => {
+  Push.create("Chat Room", {
+    body: message.text,
+    // icon: '/icon.png',
+    timeout: 4000,
+    onClick: function () {
+      window.focus();
+      this.close();
+    },
+  });
+});
